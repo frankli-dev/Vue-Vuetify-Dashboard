bb01ca081fbf347e9b639e316ba7501ac2665671
frankli <frankli.consult@gmail.com>
2019-07-17 22:10:18 +0800

    authService Scope

--- src/auth/authService.js
+++ src/auth/authService.js
@@ -7,7 +7,7 @@ const webAuth = new auth0.WebAuth({
   redirectUri: `${window.location.origin}/callback`,
   clientID: authConfig.clientId,
   responseType: "token id_token",
-  scope: "openid profile email"
+  scope: "openid"
 });
 
 const localStorageKey = "loggedIn";
